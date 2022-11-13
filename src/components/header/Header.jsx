b29f08bb45b6d51894c213335de17eb61224a49a
bilal <abii.manyun@gmail.com>
2022-11-13 23:57:20 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Muhammad Bilal Ismail</h1>
-        <h5 className="text-light">Junior Fullstack Developer</h5>
+        <h5 className="text-light">Junior Fullstack Web Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
